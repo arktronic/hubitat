1d4748ca747ad996805c8e7e98b6f18f1b7ffcd9
Sasha Kotlyar <arktronic@users.noreply.github.com>
2024-11-30 00:32:56 -0500

    Update Airthings bridge firmware code

--- http-environment-sensors/airthings/airthingsbridge.ino
+++ http-environment-sensors/airthings/airthingsbridge.ino
@@ -1,23 +1,66 @@
-#include <WiFi.h>
-#include <esp_task_wdt.h>
-#include <ArduinoBLE.h>
+/*
+ESP32 Airthings server
+
+This is part of https://github.com/arktronic/hubitat (various Hubitat Elevation projects), although it does not require the Hubitat ecosystem.
+See the README file for more info.
+
+Hardware requirements:
+- a WiFi- and BLE-capable ESP32 board
+- TFT screen (optional)
+
+Software requirements:
+- this file, opened in the Arduino IDE
+- the "ArduinoBLE" library (tested with version 1.3.7)
+- the "TFT_eSPI" library (optional; tested with version 2.5.43)
+- esp32 board support (tested with version 3.0.7)
+
+Network endpoints:
+/ - shows current status as JSON, including Airthings data when available
+/ota - allows OTA updates
+/reboot - allows to reboot the system
+
+*** IMPORTANT: Set your SSID and password below. Also, change the admin username/password, which are used for OTA and reboots.
+*/
+
+
+// -----------------------------------------
+// CONFIGURATION START
+// -----------------------------------------
 
 const char* ssid = "your ssid here";
 const char* password = "your password here";
+
+const char* adminUsername = "root";
+const char* adminPassword = "toor";
+
+// Comment this out for devices without attached screens:
+// (note: you must configure the TFT_eSPI library for your screen, if you have one!)
+#define USE_SCREEN
+
+// -----------------------------------------
+// CONFIGURATION END
+// -----------------------------------------
+
+
+#include <WiFi.h>
+#include <esp_task_wdt.h>
+#include <ArduinoBLE.h>
+#include <WebServer.h>
+#include <Update.h>
+
+#define FIRMWARE_NAME "Airthings Bridge"
+#define FIRMWARE_VERSION "v0.1.0"
 
 #define AIRTHINGS_REFRESH_TIME_MSECS (1000 * 60 * 30)
 #define AIRTHINGS_RETRY_MSECS (1000 * 30)
 #define AIRTHINGS_MAX_CONNECT_FAILURES 10
 
-// comment this out for devices without attached screens:
-// (note: you must configure the TFT_eSPI library for your screen, if you have one!)
-#define USE_SCREEN
-
 #define AIRTHINGS_BLE_SERVICE "b42e1c08-ade7-11e4-89d3-123b93f75cba"
 #define AIRTHINGS_BLE_CHARACTERISTIC "b42e2a68-ade7-11e4-89d3-123b93f75cba"
 
-WiFiServer server(80);
 BLEDevice airthingsDevice;
+
+WebServer server(80);
 
 struct AirthingsData {
   uint8_t version;
@@ -46,103 +89,172 @@ TFT_eSPI tft = TFT_eSPI();
 #endif
 
 void sos() {
-  // Flash LED until watchdog forces a reboot
-  while(true) {
-    Serial.println("SOS!");
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(800);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(800);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(800);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(2000);
-  }
+  Serial.println("SOS!");
+  digitalWrite(LED_BUILTIN, LOW);
+  esp_task_wdt_reset();
+  delay(800);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(400);
+  digitalWrite(LED_BUILTIN, HIGH);
+  esp_task_wdt_reset();
+  delay(800);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  esp_task_wdt_reset();
+  delay(800);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  esp_task_wdt_reset();
+  delay(800);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(400);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  esp_task_wdt_config_t twdt_config = {
+      .timeout_ms = 100,
+      .idle_core_mask = (1 << portNUM_PROCESSORS) - 1,    // Bitmask of all cores
+      .trigger_panic = true,
+  };
+  esp_task_wdt_reconfigure(&twdt_config);
+  delay(5000);
+  ESP.restart();
+  while(true);
 }
 
-byte processIncomingLine(String& currentLine) {
-  return (currentLine.startsWith("GET / HTTP/") ? 1 : 0);
-}
-
-void sendNotFound(WiFiClient& client) {
-  client.println("HTTP/1.1 404 Not Found");
-  client.println("Connection: close");
-  client.println("Content-Length: 0");
-  client.println();
-}
-
-void sendMessageChunk(WiFiClient& client, String chunk) {
-  client.println(String(chunk.length(), HEX));
-  client.println(chunk);
-}
-
-void sendMessageChunk(WiFiClient& client, String jsonName, double jsonValue, String suffix = ",") {
-  sendMessageChunk(client, "\"" + jsonName + "\": " + String(jsonValue) + suffix);
-}
-
-void sendMessageChunk(WiFiClient& client, String jsonName, float jsonValue, String suffix = ",") {
-  sendMessageChunk(client, "\"" + jsonName + "\": " + String(jsonValue) + suffix);
-}
-
-void sendMessageChunk(WiFiClient& client, String jsonName, int jsonValue, String suffix = ",") {
-  sendMessageChunk(client, "\"" + jsonName + "\": " + String(jsonValue) + suffix);
-}
-
-void sendMessageChunk(WiFiClient& client, String jsonName, String jsonValue, String suffix = ",") {
-  sendMessageChunk(client, "\"" + jsonName + "\": \"" + jsonValue + "\"" + suffix);
-}
-
-void sendStatus(WiFiClient& client) {
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: application/json");
-  client.println("Connection: close");
-  client.println("Transfer-Encoding: chunked");
-  client.println();
-  sendMessageChunk(client, "{");
-  sendMessageChunk(client, "last_airthings_address", airthingsAddress);
+void sendStatus() {
+  String result = "{";
+  result += "\"last_airthings_address\":\"" + String(airthingsAddress) + "\",";
   if (currentAirthingsData->version == 1) {
-    sendMessageChunk(client, "humidity_percent", currentAirthingsData->humidity / 2.0);
-    sendMessageChunk(client, "light_level", currentAirthingsData->lightLevel);
-    sendMessageChunk(client, "radon_bq_m3", currentAirthingsData->radon);
-    sendMessageChunk(client, "long_term_radon_bq_m3", currentAirthingsData->radonLongTerm);
-    sendMessageChunk(client, "temperature_c", currentAirthingsData->temperature / 100.0);
-    sendMessageChunk(client, "pressure_hpa", currentAirthingsData->pressure / 50.0);
-    sendMessageChunk(client, "co2_ppm", currentAirthingsData->co2);
-    sendMessageChunk(client, "voc_ppb", currentAirthingsData->voc);
+    result += "\"humidity_percent\":" + String(currentAirthingsData->humidity / 2.0) + ",";
+    result += "\"light_level\":" + String(currentAirthingsData->lightLevel) + ",";
+    result += "\"radon_bq_m3\":" + String(currentAirthingsData->radon) + ",";
+    result += "\"long_term_radon_bq_m3\":" + String(currentAirthingsData->radonLongTerm) + ",";
+    result += "\"temperature_c\":" + String(currentAirthingsData->temperature / 100.0) + ",";
+    result += "\"pressure_hpa\":" + String(currentAirthingsData->pressure / 50.0) + ",";
+    result += "\"co2_ppm\":" + String(currentAirthingsData->co2) + ",";
+    result += "\"voc_ppb\":" + String(currentAirthingsData->voc) + ",";
   }
-  sendMessageChunk(client, "debug_free_heap", (int)ESP.getFreeHeap());
-  sendMessageChunk(client, "debug_free_psram", (int)ESP.getFreePsram());
-  sendMessageChunk(client, "debug_uptime_hrs", (double)1 * millis() / 3600000, "");
-  sendMessageChunk(client, "}");
-  sendMessageChunk(client, "");
+  result += "\"debug_free_heap\":" + String((int)ESP.getFreeHeap()) + ",";
+  result += "\"debug_free_psram\":" + String((int)ESP.getFreePsram()) + ",";
+  result += "\"debug_uptime_hrs\":" + String((double)1 * millis() / 3600000) + ",";
+  result += "\"fw\":\"" + String(FIRMWARE_VERSION) + "\"}";
+
+  esp_task_wdt_reset();
+  server.send(200, "application/json", result);
+}
+
+
+void handleUpdateEnd() {
+  esp_task_wdt_reset();
+  server.sendHeader("Connection", "close");
+  if (Update.hasError()) {
+    server.send(500, "text/plain", Update.errorString());
+  } else {
+    server.sendHeader("Refresh", "10, url=/ota");
+    server.send(200, "text/plain", "OK. Rebooting, please wait...");
+  }
+  delay(200);
+  esp_task_wdt_reset();
+  delay(200);
+  ESP.restart();
+}
+
+void handleUpdate() {
+  size_t fsize = UPDATE_SIZE_UNKNOWN;
+  if (server.hasArg("size")) {
+    fsize = server.arg("size").toInt();
+  }
+  esp_task_wdt_reset();
+  HTTPUpload &upload = server.upload();
+  esp_task_wdt_reset();
+  if (upload.status == UPLOAD_FILE_START) {
+    Serial.printf("Receiving Update: %s, Size: %d\n", upload.filename.c_str(), fsize);
+    if (!Update.begin(fsize)) {
+      Update.printError(Serial);
+    }
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+      Update.printError(Serial);
+    } else {
+      esp_task_wdt_reset();
+    }
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (Update.end(true)) {
+      esp_task_wdt_reset();
+      Serial.printf("Update Success: %u bytes\nRebooting...\n", upload.totalSize);
+    } else {
+      Serial.printf("%s\n", Update.errorString());
+    }
+  }
+}
+
+void initWebServer() {
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    sendStatus();
+  });
+  server.on("/reboot", HTTP_GET, []() {
+    if (!server.authenticate(adminUsername, adminPassword)) {
+      return server.requestAuthentication();
+    }
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", "<form method='POST' action='/rebootnow'><input type='submit' value='Reboot now!'></form>");
+  });
+  server.on("/rebootnow", HTTP_POST, []() {
+    if (!server.authenticate(adminUsername, adminPassword)) {
+      return server.requestAuthentication();
+    }
+    server.sendHeader("Connection", "close");
+    server.sendHeader("Refresh", "10, url=/");
+    server.send(200, "text/plain", "Rebooting, please wait...");
+    delay(500);
+    ESP.restart();
+  });
+  server.on("/ota", HTTP_GET, []() {
+    if (!server.authenticate(adminUsername, adminPassword)) {
+      return server.requestAuthentication();
+    }
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", "<div>" + String(FIRMWARE_NAME) + " " + String(FIRMWARE_VERSION) + "</div><br /><form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Flash'></form>");
+  });
+  server.on(
+    "/update", HTTP_POST,
+    []() {
+      handleUpdateEnd();
+    },
+    []() {
+      if (!server.authenticate(adminUsername, adminPassword)) {
+        return server.requestAuthentication();
+      }
+      handleUpdate();
+    }
+  );
+  server.onNotFound([]() {
+    server.send(404, "text/plain", "Not Found");
+  });
+  server.begin();
 }
 
 void findAirthingsDevice() {
@@ -262,8 +374,11 @@ void updateScreenInfo() {
 #endif
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting up");
+  esp_task_wdt_deinit();
+
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("Starting up... firmware " + String(FIRMWARE_NAME) + " " + String(FIRMWARE_VERSION));
   pinMode(LED_BUILTIN, OUTPUT);
   delay(100);
 
@@ -290,23 +405,37 @@ void setup() {
   Serial.println(ssid);
 
   WiFi.setHostname("AirthingsBridge");
+  WiFi.setMinSecurity(WIFI_AUTH_WPA_PSK);
+  WiFi.mode(WIFI_STA);
+  WiFi.setAutoReconnect(true);
   WiFi.begin(ssid, password);
 
-  // If Wi-Fi doesn't connect in WDT_TIMEOUT_SECS, we'll reboot and start over
+  unsigned long wifiConnectStart = millis();
   bool ledOn = true;
   digitalWrite(LED_BUILTIN, HIGH);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  uint8_t counter = 0;
+  while (WiFi.status() != WL_CONNECTED && millis() >= wifiConnectStart && millis() < wifiConnectStart + 30000) {
+    if (counter % 50 == 0) {
+      Serial.println("WiFi status: " + String(WiFi.status()));
+    }
+    delay(50);
     ledOn = !ledOn;
     digitalWrite(LED_BUILTIN, ledOn ? HIGH : LOW);
+    esp_task_wdt_reset();
+    counter++;
   }
   digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("Connected!");
-
   esp_task_wdt_reset();
 
-  server.begin();
+  if (WiFi.status() != WL_CONNECTED) {
+    sos();
+  }
+  Serial.println("Connected!");
+
+  digitalWrite(LED_BUILTIN, LOW);
+  initWebServer();
+
+  esp_task_wdt_reset();
 
   BLE.begin();
   BLE.scanForUuid(AIRTHINGS_BLE_SERVICE);
@@ -320,38 +449,11 @@ void loop() {
 #endif
   esp_task_wdt_reset();
   delay(100);
+  esp_task_wdt_reset();
+  server.handleClient();
+  esp_task_wdt_reset();
 
   if (WiFi.status() != WL_CONNECTED) {
     sos();
-  }
-
-  WiFiClient client = server.available();
-
-  if (client) {
-    String currentLine = "";
-    byte action = 0;
-    while (client.connected()) {
-      esp_task_wdt_reset();
-      if (client.available()) {
-        char c = client.read();
-        if (c == '\r') continue;
-        if (c == '\n') {
-          if (currentLine.length() == 0) {
-            // execute current action
-            if (action == 0) {
-              sendNotFound(client);
-            } else {
-              sendStatus(client);
-            }
-            break;
-          }
-          action = action | processIncomingLine(currentLine);
-          currentLine = "";
-          continue;
-        }
-        currentLine += c;
-      }
-    }
-    client.stop();
   }
 }
