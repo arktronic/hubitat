6729c44ffbe9bc0df75e42f288eb9856dfc371eb
Sasha Kotlyar <arktronic@users.noreply.github.com>
2024-11-29 20:32:09 -0500

    Update distance sensor firmware code

--- http-distance-sensor/VL53L4CD-server.ino
+++ http-distance-sensor/VL53L4CD-server.ino
@@ -1,13 +1,52 @@
-#include <esp_task_wdt.h>
-#include <WiFi.h>
-#include <Wire.h>
-#include <vl53l4cd_class.h>
+/*
+ESP32 ToF sensor server
+
+This is part of https://github.com/arktronic/hubitat (various Hubitat Elevation projects), although it does not require the Hubitat ecosystem.
+See the README file for more info.
+
+Hardware requirements:
+- a WiFi-capable ESP32 board
+- VL53L4CD sensor
+
+Software requirements:
+- this file, opened in the Arduino IDE
+- the "STM32duino VL53L4CD" library (tested with version 1.0.5)
+- esp32 board support (tested with version 3.0.7)
+
+Network endpoints:
+/ - shows current status as JSON, including ToF distance when available
+/ota - allows OTA updates
+/reboot - allows to reboot the system
+
+*** IMPORTANT: Set your SSID and password below. Also, change the admin username/password, which are used for OTA and reboots.
+*/
+
+
+// -----------------------------------------
+// CONFIGURATION START
+// -----------------------------------------
 
 const char* ssid = "your ssid here";
 const char* password = "your password here";
 
+const char* adminUsername = "root";
+const char* adminPassword = "toor";
+
+// -----------------------------------------
+// CONFIGURATION END
+// -----------------------------------------
+
+
+#include <esp_task_wdt.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include <vl53l4cd_class.h>
+#include <WebServer.h>
+#include <Update.h>
+
+#define FIRMWARE_VERSION "v0.1.0"
+
 #define RESULTS_TOTAL 10
-#define WDT_TIMEOUT_SECS 30
 #define I2C Wire1
 #define I2C_PINS SDA1, SCL1
 #ifndef LED_BUILTIN
@@ -20,89 +59,61 @@ bool resultsReady = false;
 
 VL53L4CD tofSensor(&I2C, -1);
 
-WiFiServer server(80);
+WebServer server(80);
 
 void sos() {
-  // Flash LED until watchdog forces a reboot
-  while(true) {
-    Serial.println("SOS!");
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(800);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(800);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(800);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(2000);
-  }
-}
+  Serial.println("SOS!");
+  digitalWrite(LED_BUILTIN, LOW);
+  esp_task_wdt_reset();
+  delay(800);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(400);
+  digitalWrite(LED_BUILTIN, HIGH);
+  esp_task_wdt_reset();
+  delay(800);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  esp_task_wdt_reset();
+  delay(800);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  esp_task_wdt_reset();
+  delay(800);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(400);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
 
-byte processIncomingLine(String& currentLine) {
-  return (currentLine.startsWith("GET / HTTP/") ? 1 : 0);
-}
-
-void sendNotFound(WiFiClient& client) {
-  client.println("HTTP/1.1 404 Not Found");
-  client.println("Connection: close");
-  client.println("Content-Length: 0");
-  client.println();
-}
-
-void sendMessageChunk(WiFiClient& client, String chunk) {
-  client.println(String(chunk.length(), HEX));
-  client.println(chunk);
-}
-
-void sendMessageChunk(WiFiClient& client, String jsonName, double jsonValue, String suffix = ",") {
-  sendMessageChunk(client, "\"" + jsonName + "\": " + String(jsonValue) + suffix);
-}
-
-void sendMessageChunk(WiFiClient& client, String jsonName, float jsonValue, String suffix = ",") {
-  sendMessageChunk(client, "\"" + jsonName + "\": " + String(jsonValue) + suffix);
-}
-
-void sendMessageChunk(WiFiClient& client, String jsonName, int jsonValue, String suffix = ",") {
-  sendMessageChunk(client, "\"" + jsonName + "\": " + String(jsonValue) + suffix);
-}
-
-void sendMessageChunk(WiFiClient& client, String jsonName, uint16_t jsonValue, String suffix = ",") {
-  sendMessageChunk(client, "\"" + jsonName + "\": " + String(jsonValue) + suffix);
-}
-
-void sendMessageChunk(WiFiClient& client, String jsonName, bool jsonValue, String suffix = ",") {
-  sendMessageChunk(client, "\"" + jsonName + "\": " + (jsonValue ? "true" : "false") + suffix);
-}
-
-void sendMessageChunk(WiFiClient& client, String jsonName, String jsonValue, String suffix = ",") {
-  sendMessageChunk(client, "\"" + jsonName + "\": \"" + jsonValue + "\"" + suffix);
+  esp_task_wdt_config_t twdt_config = {
+      .timeout_ms = 100,
+      .idle_core_mask = (1 << portNUM_PROCESSORS) - 1,    // Bitmask of all cores
+      .trigger_panic = true,
+  };
+  esp_task_wdt_reconfigure(&twdt_config);
+  delay(5000);
+  ESP.restart();
+  while(true);
 }
 
 bool getDistance(bool& validated, uint16_t& distance_mm) {
@@ -139,26 +150,113 @@ bool getDistance(bool& validated, uint16_t& distance_mm) {
   return false;
 }
 
-void sendStatus(WiFiClient& client) {
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: application/json");
-  client.println("Connection: close");
-  client.println("Transfer-Encoding: chunked");
-  client.println();
-  sendMessageChunk(client, "{");
-
+void sendStatus() {
+  String result = "{";
   bool validated;
   uint16_t distance_mm;
   bool gotDistance = getDistance(validated, distance_mm);
   if (gotDistance) {
-    sendMessageChunk(client, "distance_validated", validated);
-    sendMessageChunk(client, "distance_mm", distance_mm);
+    result += "\"distance_validated\":" + String(validated ? "true" : "false") + ",";
+    result += "\"distance_mm\":" + String(distance_mm) + ",";
   }
-  sendMessageChunk(client, "debug_free_heap", (int)ESP.getFreeHeap());
-  sendMessageChunk(client, "debug_free_psram", (int)ESP.getFreePsram());
-  sendMessageChunk(client, "debug_uptime_hrs", (double)1 * millis() / 3600000, "");
-  sendMessageChunk(client, "}");
-  sendMessageChunk(client, "");
+  result += "\"debug_free_heap\":" + String((int)ESP.getFreeHeap()) + ",";
+  result += "\"debug_free_psram\":" + String((int)ESP.getFreePsram()) + ",";
+  result += "\"debug_uptime_hrs\":" + String((double)1 * millis() / 3600000) + ",";
+  result += "\"fw\":\"" + String(FIRMWARE_VERSION) + "\"}";
+
+  esp_task_wdt_reset();
+  server.send(200, "application/json", result);
+}
+
+void handleUpdateEnd() {
+  esp_task_wdt_reset();
+  server.sendHeader("Connection", "close");
+  if (Update.hasError()) {
+    server.send(500, "text/plain", Update.errorString());
+  } else {
+    server.sendHeader("Refresh", "10, url=/ota");
+    server.send(200, "text/plain", "OK. Rebooting, please wait...");
+  }
+  delay(200);
+  esp_task_wdt_reset();
+  delay(200);
+  ESP.restart();
+}
+
+void handleUpdate() {
+  size_t fsize = UPDATE_SIZE_UNKNOWN;
+  if (server.hasArg("size")) {
+    fsize = server.arg("size").toInt();
+  }
+  esp_task_wdt_reset();
+  HTTPUpload &upload = server.upload();
+  esp_task_wdt_reset();
+  if (upload.status == UPLOAD_FILE_START) {
+    Serial.printf("Receiving Update: %s, Size: %d\n", upload.filename.c_str(), fsize);
+    if (!Update.begin(fsize)) {
+      Update.printError(Serial);
+    }
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+      Update.printError(Serial);
+    } else {
+      esp_task_wdt_reset();
+    }
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (Update.end(true)) {
+      esp_task_wdt_reset();
+      Serial.printf("Update Success: %u bytes\nRebooting...\n", upload.totalSize);
+    } else {
+      Serial.printf("%s\n", Update.errorString());
+    }
+  }
+}
+
+void initWebServer() {
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    sendStatus();
+  });
+  server.on("/reboot", HTTP_GET, []() {
+    if (!server.authenticate(adminUsername, adminPassword)) {
+      return server.requestAuthentication();
+    }
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", "<form method='POST' action='/rebootnow'><input type='submit' value='Reboot now!'></form>");
+  });
+  server.on("/rebootnow", HTTP_POST, []() {
+    if (!server.authenticate(adminUsername, adminPassword)) {
+      return server.requestAuthentication();
+    }
+    server.sendHeader("Connection", "close");
+    server.sendHeader("Refresh", "10, url=/");
+    server.send(200, "text/plain", "Rebooting, please wait...");
+    delay(500);
+    ESP.restart();
+  });
+  server.on("/ota", HTTP_GET, []() {
+    if (!server.authenticate(adminUsername, adminPassword)) {
+      return server.requestAuthentication();
+    }
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", "<div>" + String(FIRMWARE_VERSION) + "</div><br /><form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Flash'></form>");
+  });
+  server.on(
+    "/update", HTTP_POST,
+    []() {
+      handleUpdateEnd();
+    },
+    []() {
+      if (!server.authenticate(adminUsername, adminPassword)) {
+        return server.requestAuthentication();
+      }
+      handleUpdate();
+    }
+  );
+  server.onNotFound([]() {
+    server.send(404, "text/plain", "Not Found");
+  });
+  server.begin();
 }
 
 void setup() {
@@ -169,31 +267,51 @@ void setup() {
   delay(10);
 
   // Initialize watchdog
-  esp_task_wdt_init(WDT_TIMEOUT_SECS, true);
+  esp_task_wdt_config_t twdt_config = {
+      .timeout_ms = 30000,
+      .idle_core_mask = (1 << portNUM_PROCESSORS) - 1,    // Bitmask of all cores
+      .trigger_panic = true,
+  };
+  esp_task_wdt_init(&twdt_config);
   esp_task_wdt_add(NULL);
 
   WiFi.setMinSecurity(WIFI_AUTH_WPA_PSK);
+  WiFi.mode(WIFI_STA);
+  WiFi.setAutoReconnect(true);
   WiFi.begin(ssid, password);
 
-  // If Wi-Fi doesn't connect in WDT_TIMEOUT_SECS, we'll reboot and start over
+  unsigned long wifiConnectStart = millis();
   bool ledOn = true;
   digitalWrite(LED_BUILTIN, HIGH);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  uint8_t counter = 0;
+  while (WiFi.status() != WL_CONNECTED && millis() >= wifiConnectStart && millis() < wifiConnectStart + 30000) {
+    if (counter % 50 == 0) {
+      Serial.println("WiFi status: " + String(WiFi.status()));
+    }
+    delay(50);
     ledOn = !ledOn;
     digitalWrite(LED_BUILTIN, ledOn ? HIGH : LOW);
+    esp_task_wdt_reset();
+    counter++;
   }
   digitalWrite(LED_BUILTIN, LOW);
+  esp_task_wdt_reset();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    sos();
+  }
+  
+  digitalWrite(LED_BUILTIN, LOW);
+  initWebServer();
 
   esp_task_wdt_reset();
-  
-  server.begin();
 
   tofSensor.begin();
   tofSensor.VL53L4CD_Off();
-  tofSensor.InitSensor();
-  tofSensor.VL53L4CD_SetRangeTiming(200, 0);
-  tofSensor.VL53L4CD_StartRanging();
+  if (tofSensor.InitSensor() == VL53L4CD_ERROR_NONE) {
+    tofSensor.VL53L4CD_SetRangeTiming(200, 0);
+    tofSensor.VL53L4CD_StartRanging();
+  }
 }
 
 void processDistanceReading() {
@@ -224,38 +342,10 @@ void loop() {
   processDistanceReading();
   delay(200);
   esp_task_wdt_reset();
+  server.handleClient();
+  esp_task_wdt_reset();
 
   if (WiFi.status() != WL_CONNECTED) {
     sos();
-  }
-
-  WiFiClient client = server.available();
-
-  if (client) {
-    String currentLine = "";
-    byte action = 0;
-    while (client.connected()) {
-      esp_task_wdt_reset();
-      if (client.available()) {
-        char c = client.read();
-        if (c == '\r') continue;
-        if (c == '\n') {
-          if (currentLine.length() == 0) {
-            // execute current action
-            if (action == 0) {
-              sendNotFound(client);
-            } else {
-              sendStatus(client);
-            }
-            break;
-          }
-          action = action | processIncomingLine(currentLine);
-          currentLine = "";
-          continue;
-        }
-        currentLine += c;
-      }
-    }
-    client.stop();
   }
 }
